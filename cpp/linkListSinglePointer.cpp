4214c74164fa8f60af0863cd1737ad05ac7324e9
abhiksark <abhiksark@gmail.com>
2017-07-10 17:35:23 +0530

    First Link List

--- cpp/linkListSinglePointer.cpp
+++ cpp/linkListSinglePointer.cpp
@@ -1,1 +1,20 @@
 #include<iostream>
+using namespace std;
+struct node{
+	int data;
+	node* link;
+};
+node* createNode(int);
+node* addToHead(node*,int);
+void printAll(node*);
+int main(){
+	node *head = NULL;
+	head=addToHead(head,10);
+	return 0;
+}
+node* createNode(int data){
+	node newNode = new Node;
+	newNode->link = NULL; newNode->data = data;
+	return newNode;
+}
+node* addToHead(node* head, int data){}
