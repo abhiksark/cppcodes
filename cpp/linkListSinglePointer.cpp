55edb4ad8a1666839824c04cb310a8a8fca79b3e
abhiksark <abhiksark@gmail.com>
2017-07-11 19:52:17 +0530

    added linklist

--- cpp/linkListSinglePointer.cpp
+++ cpp/linkListSinglePointer.cpp
@@ -6,12 +6,18 @@ struct node{
 };
 node* createNode(int);
 node* addToHead(node*,int);
+node* addToTail(node*,int);
+node* deleteTail(node*);
+node* deleteHead(node*);
 void printAll(node*);
 int main(){
 	node* head = NULL;
 	head=addToHead(head,10);head=addToHead(head,15);
-	head=addToHead(head,14);head=addToHead(head,25);	
-	printAll(head);	
+	head=addToHead(head,14);head=addToHead(head,25);
+	head=addToTail(head,-15);head=addToTail(head,-10);
+    printAll(head); cout<<endl;
+    head=deleteTail(head); head = deleteHead(head);
+	printAll(head);
 	return 0;
 }
 node* createNode(int data){
@@ -25,7 +31,7 @@ node* addToHead(node* head, int data){
 		head= newNode;
 		return head;
 	}
-	newNode->link = head; 
+	newNode->link = head;
 	head = newNode;
 	return head;
 }
@@ -34,4 +40,38 @@ void printAll(node* head){
 		cout<<head->data<<" ";
 		head = head->link;
 	}
+}
+node* addToTail(node* head, int data){
+    node* newNode = createNode(data);
+    node* traverse = head;
+    if(head==NULL){
+        head = newNode;
+        return head;
+    }
+    while(traverse->link!=NULL){traverse= traverse->link;}
+    traverse->link = newNode;
+    return head;
+}
+node* deleteTail(node* head){
+    node* current = head;
+    node* prev = head;
+    if(head==NULL){
+        return head;
+    }
+    while(current->link!=NULL){
+        prev=current;
+        current= current->link;
+    }
+    prev->link=NULL;
+    delete current;
+    return head;
+}
+node* deleteHead(node* head){
+    node* current = head;
+    if(head==NULL){
+        return head;
+    }
+    head = current->link;
+    delete current;
+    return head;
 }
