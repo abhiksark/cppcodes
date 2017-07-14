51f5b795962c3c5d19e7a4ab31ccd1d4f534a6e3
abhiksark <abhiksark@gmail.com>
2017-07-14 16:49:44 +0530

    added heap sort

--- cpp/binaryTreeUnbalanced.cpp
+++ cpp/binaryTreeUnbalanced.cpp
@@ -10,13 +10,20 @@ node *addNode(node*,int);
 void printPostOrder(node*);
 void printPreOrder(node*);
 void printInOrder(node*);
+int findNode(node*,int);
+node* findMin(node*);
+node* deleteNode(node*, int);
 int main(void){
     node* head = NULL;
     head = addNode(head,10);head = addNode(head,03);
     head = addNode(head,0);head = addNode(head,30);
+    head = addNode(head,80);head = addNode(head,13);
+    head = addNode(head,7);head = addNode(head,06);
     printInOrder(head);cout<<endl;
-    printPreOrder(head);cout<<endl;
-    printPostOrder(head);cout<<endl;
+    //printPreOrder(head);cout<<endl;
+    //printPostOrder(head);cout<<endl;
+    head =deleteNode(head,30);
+    printInOrder(head);cout<<endl;
     return 0;
 }
 node *createNode(int data){
@@ -58,4 +65,50 @@ void printPostOrder(node *head){
     printPostOrder(head->left);
     printPostOrder(head->right);
     cout<<head->data<<" ";
+}
+int findNode(node* head,int data){
+    if(head==NULL)
+        return -1;
+    if(data == head->data)
+        return data;
+    if(data < head->data)
+        return findNode(head->left,data);
+    if(data> head->data) 
+        return findNode(head->right,data);
+}
+node *findMin(node* head){
+    while(head->left!=NULL){
+        head = head->left;
+    }
+    return head;
+
+}
+node* deleteNode(node* head, int data){
+    
+    if(head==NULL)
+        return head;
+    else if(data < head->data)
+        head = deleteNode(head->left,data);
+    else if(data> head->data) 
+        head = deleteNode(head->right,data);
+
+    else{    // found the child 
+        if(head->left==NULL && head->right == NULL){ //no child 
+            delete head;
+        }
+        else if(head->left==NULL){ //no left child
+            head = head->right;
+        }
+        else if(head->right == NULL){ //no right child
+            head = head->left;
+        }
+        else{
+            node* temp = findMin(head->right);
+            head->data = temp->data;
+            head->right = deleteNode(head->right,temp->data);
+        }
+
+    }
+
+    return head;
 }
